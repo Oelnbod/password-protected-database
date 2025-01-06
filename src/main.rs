ca9059378af1036fd7e6c389ca1225b08b0f1ccb
joel body <joel.body@proton.me>
2025-01-06 12:09:34 +0000

    added code to encrpyt a string using aes-gcm crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+// DOnt forget to git commit
+use aes_gcm::{
+    aead::{Aead, AeadCore, KeyInit, OsRng}, Aes256Gcm, AesGcm, Key, Nonce
+};
+//https://backendengineer.io/aes-encryption-rust
 fn main() {
-    println!("Hello, world!");
+    let plaintext = "hi".to_string();
+    let key_str = "thiskeystrmustbe32charlongtowork";
+    encrypt_data(plaintext, key_str);
+
+}
+
+fn encrypt_data(plaintext: String, key_str: String) -> Vec<u8> {
+    let key =  Key::<Aes256Gcm>::from_slice(key_str.as_bytes());
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+    let cipher = Aes256Gcm::new(key);
+    let ciphertext = cipher.encrypt(&nonce, plaintext.as_bytes()).expect("err");
+    let mut encrypted_data: Vec<u8> = nonce.to_vec();
+    encrypted_data.extend_from_slice(&ciphertext);
+    encrypted_data
+    
+
 }
